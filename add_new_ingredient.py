6ecb6f045a44bbf8c36866eb872a2dd44064770e
Michael Sun <michaelsun18@yahoo.com>
2018-10-25 22:50:50 -0700

    Changed key

--- add_new_ingredient.py
+++ add_new_ingredient.py
@@ -10,15 +10,19 @@ def add_new(x):
         
             'Content-Type': 'application/json',
             'Accept': 'application/json',
-            'x-app-id': '27103a3a',
-            'x-app-key': '362533bdddd91cb1453b5d2ce28cd5b3',
+            'x-app-id': 'e0d2583c',
+            'x-app-key': '992176a868ced191c469ea0df71c7a54',
             'x-remote-user-id': '0',
         }
 
     data = {"query": x}
     r = requests.post(url, headers = headers, data = json.dumps(data))
     body = json.loads(r.text)
-    food = body['foods'][0]
+
+    try: 
+        food = body['foods'][0]
+    except KeyError:
+        return jsonify("OH NO")
 
     dic = {'label': str(x),
            'amount': str(food["serving_qty"]) + ' ' + str(food["serving_unit"]),
