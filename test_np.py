05256d618c505cd0d17d04262f3a7cfad1c87edf
Michael Sun <michaelsun18@yahoo.com>
2018-11-02 06:31:08 -0700

    Yea

--- test_np.py
+++ test_np.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
 
-df_nutrition = pd.read_csv('./new_laplacian_matrix.csv').iloc[:-1, 1:-1]
+df_nutrition = pd.read_csv('./new_ingredients_test.csv').loc[:,'Ingredient':]
 print(df_nutrition)
-df_nutrition.to_csv('./new_laplacian_matrix.csv')
