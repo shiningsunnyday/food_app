6a52b18833b04663ed0a770eb9f3fb4b29698168
Michael Sun <michaelsun18@yahoo.com>
2018-11-05 22:35:34 -0800

    Int to float

--- add_new_ingredient.py
+++ add_new_ingredient.py
@@ -22,8 +22,9 @@ def add_new(x, boo):
 
     data = {"query": x}
     r = requests.post(url, headers = headers, data = json.dumps(data))
+    print(r)
     body = json.loads(r.text)
-
+    print(body)
     try: 
         food = body['foods'][0]
     except KeyError:
@@ -31,10 +32,10 @@ def add_new(x, boo):
 
     dic = {'label': str(x),
            'amount': str(food["serving_qty"]) + ' ' + str(food["serving_unit"]),
-           'calories': int(food["nf_calories"]),
-           'protein': int(food["nf_protein"]),
-           'fat': int(food["nf_total_fat"]),
-           'carbs': int(food["nf_total_carbohydrate"]),
+           'calories': float(food["nf_calories"]),
+           'protein': float(food["nf_protein"]),
+           'fat': float(food["nf_total_fat"]),
+           'carbs': float(food["nf_total_carbohydrate"]),
            }
 
     if boo:
