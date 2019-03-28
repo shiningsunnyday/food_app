2a2f4e3ea972c6c6736cb69fcbb9008b26007eb0
shiningsunnyday <37891012+shiningsunnyday@users.noreply.github.com>
2019-03-27 21:06:57 -0700

    Long commit; adding Jupyter Notebook work and processed csv files

--- __init__.py
+++ __init__.py
@@ -1,10 +1,7 @@
 from flask import Flask
-from flask_cors import CORS
+from config import Config
 
 app = Flask(__name__)
-CORS(app)
+app.config.from_object(Config)
 
-from app import routes
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+from food_app_api import app
